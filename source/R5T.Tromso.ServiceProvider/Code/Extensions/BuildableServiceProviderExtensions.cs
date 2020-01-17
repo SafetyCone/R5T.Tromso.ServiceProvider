9a5ef7eee7ee3f74f02bda238b0689de877b6c38
MinexAutomation <minex.automation@gmail.com>
2020-01-16 22:54:41 -0800

    Improved UseStartup() method.

--- source/R5T.Tromso.ServiceProvider/Code/Extensions/BuildableServiceProviderExtensions.cs
+++ source/R5T.Tromso.ServiceProvider/Code/Extensions/BuildableServiceProviderExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 
 using R5T.Richmond;
-using R5T.Tromso.Startup;
-using R5T.Tromso.Types;
 
 
 namespace R5T.Tromso.ServiceProvider
@@ -12,11 +10,7 @@ namespace R5T.Tromso.ServiceProvider
         public static IServiceProvider UseStartup<TStartup>(this BuildableServiceProvider buildableServiceProvider, IServiceProvider configurationServiceProvider)
             where TStartup : class, IStartup
         {
-            var serviceBuilder = buildableServiceProvider.GetServiceBuilder();
-
-            serviceBuilder.UseStartup<TStartup>();
-
-            serviceBuilder.Build(configurationServiceProvider);
+            buildableServiceProvider.UseStartup<TStartup>(configurationServiceProvider);
 
             var serviceProvider = buildableServiceProvider.GetResult();
             return serviceProvider;
