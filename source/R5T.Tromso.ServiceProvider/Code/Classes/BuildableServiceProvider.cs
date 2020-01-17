c519c39020d04400be9074f78d04b5f10dfd14c8
MinexAutomation <minex.automation@gmail.com>
2020-01-16 17:38:06 -0800

    Added a service provider buildable and service builder extensions to build a service provider.

---
+++ source/R5T.Tromso.ServiceProvider/Code/Classes/BuildableServiceProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Exeter;
+
+
+namespace R5T.Tromso.ServiceProvider
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// Placed the <see cref="BuildableServiceProvider"/> in the base <see cref="R5T.Tromso"/> namespace to simplify client using statment, since adding a reference to this library is enough to indicate a desire to use.
+    /// </remarks>
+    public class BuildableServiceProvider : IBuildableService
+    {
+        private IConfigurationBuilder zConfigurationBuilder;
+        public IConfigurationBuilder ConfigurationBuilder
+        {
+            get
+            {
+                this.zConfigurationBuilder = new ConfigurationBuilder();
+
+                return this.zConfigurationBuilder;
+            }
+        }
+        private IServiceCollection zServiceCollection;
+        public IServiceCollection ServiceCollection
+        {
+            get
+            {
+                var configuration = this.zConfigurationBuilder.Build();
+
+                this.zServiceCollection = new ServiceCollection()
+                    .AddConfiguration(configuration)
+                    ;
+
+                return this.zServiceCollection;
+            }
+        }
+        private IServiceProvider zServiceProvider;
+        public IServiceProvider ServiceProvider
+        {
+            get
+            {
+                this.zServiceProvider = this.zServiceCollection.BuildServiceProvider();
+
+                return this.zServiceProvider;
+            }
+        }
+
+
+        public IServiceProvider GetResult()
+        {
+            return this.zServiceProvider;
+        }
+    }
+}
---
+++ source/R5T.Tromso.ServiceProvider/Code/Extensions/IServiceBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using R5T.Richmond;
+using R5T.Tromso.Startup;
+
+
+namespace R5T.Tromso.ServiceProvider
+{
+    public static class IServiceBuilderExtensions
+    {
+        public static IServiceProvider BuildServiceProvider(this IServiceBuilder serviceBuilder, IServiceProvider configurationServiceProvider)
+        {
+            var buildableServiceProvider = new BuildableServiceProvider();
+
+            serviceBuilder.Build(buildableServiceProvider, configurationServiceProvider);
+
+            var outputServiceProvider = buildableServiceProvider.GetResult();
+            return outputServiceProvider;
+        }
+
+        public static IServiceProvider UseStartup<TStartup>(this IServiceBuilder serviceBuilder, IServiceProvider configurationServiceProvider)
+            where TStartup : class, IStartup
+        {
+            serviceBuilder.UseStartup<TStartup>();
+
+            var serviceProvider = serviceBuilder.BuildServiceProvider(configurationServiceProvider);
+            return serviceProvider;
+        }
+    }
+}
